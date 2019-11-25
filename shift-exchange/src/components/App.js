a5c805733b4157591288999d42862c11ed25b575
rdollent <rumbalumba89@gmail.com>
2019-11-24 23:18:58 -0700

    prelim commit for company

--- shift-exchange/src/components/App.js
+++ shift-exchange/src/components/App.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD:shift-exchange/src/components/App.js
 import React from 'react';
 import logo from '../styles/logo.svg';
 import '../styles/styles.scss';
-=======
-import React from 'react'
-import './App.css'
->>>>>>> origin/development:shift-exchange/src/App.js
 
 function App() {
   return (
     <div>
-      Welcome to Shift Portal Exchange
+      <div>For Companies</div>
+      <div>For Employees</div>
     </div>    
   )
 }
