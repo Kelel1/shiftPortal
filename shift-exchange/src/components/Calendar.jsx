c7d0cab66fa98351c4071d9c9cb5e766c011da78
rdollent <rumbalumba89@gmail.com>
2019-12-12 23:07:00 -0700

    added reducer, action, store

--- shift-exchange/src/components/Calendar.jsx
+++ shift-exchange/src/components/Calendar.jsx
@@ -4,7 +4,16 @@ class Calendar extends React.Component {
     constructor(props) {
         super(props);
         this.days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+        this.months = [
+            'January', 'February', 'March', 'April',
+            'May', 'June', 'July', 'August', 'September',
+            'October', 'November', 'December'
+        ];
         this.state = {
+            currentMonth: 0,
+            firstDay: '',
+            lastDate: '',
+            lastDay: '',
 
         }
     }
@@ -25,13 +34,16 @@ class Calendar extends React.Component {
 
     getCurrentMonth() {
         let x = new Date();
-        return x.getMonth();
+        this.setState({ currentMonth: x.getMonth()});
     }
 
     //get # of weeks
     // count 2 weeks straight off-the-bat. 1st week where first date of month is.
     // 2nd week is where last date of month is.
-    // take 
+    // take first date of the last week, subtract 1 to get the last date of the week before
+    // i.e. if 27 is first date of last week, then the 26th is the last date of the week before
+    // after getting that date, look for the last date of the first week
+    // then subtract that from the last date of 2nd-to-the-last week
     getNumberOfWeeks() {
 
 
